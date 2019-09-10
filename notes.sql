bb9b798f268b4fd59b6ae6acfb06e1dcee83fcb8
dasong2410 <dasong2410@gmail.com>
2019-09-10 09:46:29 +0800

    hi

--- notes.sql
+++ notes.sql
@@ -80,23 +80,11 @@ SELECT DATE_ADD('2016-08-02', INTERVAL help_topic_id DAY) as mydate
 FROM mysql.help_topic order by help_topic_id asc limit 14;
 
 
--- issues
--- 1. 字符集导致数据插入失败，修改表字符集后成功
-drop table if exists tmp_charset_test_for_drop;
-create table tmp_charset_test_for_drop
-(
-    c1 varchar(255)
-);
+# 创建用户
+create user dasong@'%' identified by '2wsx@WSX';
 
-insert into tmp_charset_test_for_drop values('🙃');
-[2019-08-09 09:23:44] [HY000][1366] Incorrect string value: '\xF0\x9F\x99\x83' for column 'c1' at row 1
+# 赋权限
+grant all privileges on *.* to 'dasong'@'%';
 
-ALTER TABLE tmp_charset_test_for_drop CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-insert into tmp_charset_test_for_drop values('🙃');
-[2019-08-09 09:24:46] 1 row affected in 12 ms
-
-
--- 2. mysql 表可以设置区分大小写，linux默认区分，windows默认不区分，操蛋的功能
--- 3. mysql 中没有区间值生成函数，如生成 1-10000 的连续数字，需要自己写函数
--- 4. mysql 社区版没有备份功能
--- 5. mysql 表可以单独设置字符集，不知道实际有没有用
+# 备份
+mysqldump -uroot -p ODS > dump_$(date +"%Y%m%d").sql
